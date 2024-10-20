bb11d18210cd55d9caee33896dcfa770053e99ae
klasmer <klasmer495@gmail.com>
2024-10-20 13:36:01 +0300

    Вводное в Maven и Spring Boot + web

---
+++ src/main/java/com/calculator/hw/CalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWebApplication.class, args);
+	}
+
+}
