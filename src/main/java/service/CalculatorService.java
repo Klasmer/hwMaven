3922a2455b0abe7c5d8e535138edb1e299ef2907
klasmer <klasmer495@gmail.com>
2024-10-21 21:38:48 +0300

    Вводное в Maven и Spring Boot + web. Помощь

---
+++ src/main/java/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface CalculatorService {
+    Integer sum(Integer num1, Integer num2);
+    Integer subtract(Integer num1, Integer num2);
+    Integer multiply(Integer num1, Integer num2);
+    Integer divide(Integer num1, Integer num2);
+}
